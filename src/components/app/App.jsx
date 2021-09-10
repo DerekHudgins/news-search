9923dbe9ee86cb6486438a71a7633d3fcb020d2a
Derek Hudgins <hudgins.derek@gmail.com>
2021-09-10 09:34:48 -0700

    created container/NewsSearch

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import  React, { Component }  from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NewsSearch from '../../containers/NewsSearch';
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
     return (
       <>
